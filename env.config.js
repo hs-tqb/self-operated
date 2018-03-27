73e01d08b9b82a58a4d87c4cd29a943c8065b0ab
hs <huangshuai@utianqi.com>
2018-03-27 16:26:38 +0800

    切回host

--- env.config.js
+++ env.config.js
@@ -1,11 +1,11 @@
 let config = {
   development: {
-    // baseURL: 'http://test.selfrain.baotianqi.cn/'
-    baseURL: 'http://192.168.1.159:8008/'
+    baseURL: 'http://test.selfrain.baotianqi.cn/'
+    // baseURL: 'http://192.168.1.159:8008/'
   },
   test: {
-    // baseURL: 'http://test.selfrain.baotianqi.cn/'
-    baseURL: 'http://192.168.1.159:8008/'
+    baseURL: 'http://test.selfrain.baotianqi.cn/'
+    // baseURL: 'http://192.168.1.159:8008/'
   },
   production: {
     baseURL: 'http://web.baotianqi.cn/'
