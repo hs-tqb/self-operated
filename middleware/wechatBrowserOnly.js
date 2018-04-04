5a8ee5af992b035b9083dcffa98e395c0e28328d
HS <huangshuai@utianqi.com>
2018-04-04 15:47:28 +0800

    对非正是环境不做限制

--- middleware/wechatBrowserOnly.js
+++ middleware/wechatBrowserOnly.js
@@ -1,8 +1,9 @@
 export default function ( { req, redirect} ) {
+  // 测试环境不做跳转
+  if ( process.env.NODE_ENV === 'development' ) return;
+  // 正式环境下, 只允许微信打开  
   let userAgent = process.server? req.headers['user-agent']: navigator.userAgent;
-  // 对浏览器的UserAgent进行正则匹配，不含有微信独有标识的则为其他浏览器  
-  if (userAgent.match(/MicroMessenger/i) != 'MicroMessenger') {
-    // 这里警告框会阻塞当前页面继续加载  
+  if ( userAgent.match(/MicroMessenger/i) != 'MicroMessenger') {
     redirect('/wechatBrowserOnly');
   }
 };
